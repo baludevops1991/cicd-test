25cb4d844861317ba02b4d4ce23aeee5192ffb23
Balu Sreeramula <balusreeramula@gmail.com>
2025-04-23 12:25:29 +0530

    Added Subnets

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,5 @@
 region                      = "ap-south-1"
 availability_zones          = ["ap-south-1a", "ap-south-1b"]
 vpc_cidr                    = "10.0.0.0/16"
+public_subnet_cidrs         = ["10.0.1.0/24", "10.0.2.0/24"]
+private_subnet_cidrs        = ["10.0.10.0/24", "10.0.20.0/24"]
--- variales.tf
+++ variales.tf
@@ -12,3 +12,18 @@ variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
 }
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+}
